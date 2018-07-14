c5ae6a3c87bfea176b558e3547b48321eb1140cc
Matthew Buchanan <mwbuchan92@gmail.com>
2018-07-14 16:27:56 -0400

    sample files for folders

---
+++ client/src/components/NavExpand/NavExpand.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NavExpand.css';
+
+const NavExpand = props => {
+  render(/*JSX*/);
+}
+
+export default NavExpand;
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render(/*JSX*/);
+}
+export default Home;
