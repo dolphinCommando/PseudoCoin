ae2e83aa6ef504a4d5a053d1e1541e38ad263c9f
Matthew Buchanan <mwbuchan92@gmail.com>
2018-07-17 18:52:36 -0400

    Installed nodemon and concurrently; added proxy on port 3001 in client package.json; NavExpand.js render() removed because static element

--- client/src/components/NavExpand/NavExpand.js
+++ client/src/components/NavExpand/NavExpand.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './NavExpand.css';
 
+const NavExpand = props => (
+  <div></div>
+);
+
+/*
+if you need to do logic on props, try
 const NavExpand = props => {
-  render(/*JSX*/);
+  const newProps = <do stuff to props>
+  return (
+    <div>{newProps.exampleProperty}</div>
+  )
 }
+*/
 
 export default NavExpand;
