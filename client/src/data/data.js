a4cd511d3e4daf22f634ff454ac60392c46c8f5d
dolphinCommando <33132422+dolphinCommando@users.noreply.github.com>
2018-07-18 13:46:35 -0400

    Update data.js
    change --> percent for clarification

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -5,12 +5,12 @@ const mockData = {
       history: ["7352.17", "7365.25", "7289.51", "7349.40"]
     },
     litecoin: {
-      change: "6.64",
+      percent: "6.64",
       price: "89.94",
       history: ["89.94", "78.47", "119.50", "162.88"]
     },
     ethereum: {
-      change: "5.26",
+      percent: "5.26",
       price: "504.18",
       history: ["504.18", "443.02", "491.74", "433.67"]
     }
