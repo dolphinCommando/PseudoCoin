c93107ddc219ed75a4bbdbb0e8bd62bd17bf9495
Matthew Buchanan <mwbuchan92@gmail.com>
2018-07-19 11:39:56 -0400

    Changed chartjs to react-chart-js2; chart renders on page; tweaks to NavExpand

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import NavExpand from './components/NavExpand';
+import NavExpand from './components/NavExpand';
 import Home from './pages/Home';
 import './App.css';
 
@@ -11,12 +11,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">PseudoCoin</h1>
-          <Home />
-          <NavExpand />
         </header>
         <p className="App-intro">
           Welcome to PseudoCoin! Click on the "Current Amount" or "Investments" section, or click on the side navigation bar for "Subscriptions", "Manage", "User Profile", or "Notifications".
         </p>
+        <Home />
+        <NavExpand />
       </div>
     );
   }
