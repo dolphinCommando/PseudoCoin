b7f7285824752ce0de0933926b010a1de51162be
Matthew Buchanan <mwbuchan92@gmail.com>
2018-07-21 10:36:16 -0400

    axios for client

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,1 +1,19 @@
 import axios from 'axios';
+
+export default {
+  getTrades: function() {
+    axios.get('/api/coins');
+  },
+  findTrade: function(id) {
+    axios.get('/api/coins/:id');
+  },
+  deleteTrade: function(id) {
+    axios.delete('/api/coins/:id');
+  },
+  updateTrade: function(id) {
+    axios.put('/api/coins/:id');
+  },
+  getCryptoData: function() {
+    axios.get('/api/crypto');
+  }
+}
