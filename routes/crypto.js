5df9b2389563d7e86be783c99e5dddba866623f3
Matthew Buchanan <mwbuchan92@gmail.com>
2018-07-21 13:55:31 -0400

    requestCrypto function with callback

--- routes/crypto.js
+++ routes/crypto.js
@@ -1,8 +1,26 @@
 var request = require('request');
+
 var coinlistURL = 'https://www.cryptocompare.com/api/data/coinlist/';
-var priceURL = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,LTC,USD,EUR';
-  request(priceURL, function(error, response, body) {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode); 
-    console.log('body:', JSON.parse(body)); 
-  })
+// Be aware that the # of currencies in CryptoCompare is 2842
+var priceURLTest = 'https://min-api.cryptocompare.com/data/price?fsym=BTG&tsyms=BTC,LTC,ETH,USD';
+var CRYPTO = 'https://min-api.cryptocompare.com/data/';
+var PRICE_URL = CRYPTO += 'price';
+var PRICE_HIST_URL = CRYPTO += 'pricehistorical';
+var HISTO_MIN_URL = CRYPTO += 'histominute';
+var HISTO_HOUR_URL = CRYPTO += 'histohour';
+var HISTO_DAY_URL = CRYPTO += 'histoday';
+
+function requestCrypto(url, cb) {
+  request(url, function(error, response, body) {
+    if (error) {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);  
+    }
+    cb(body);
+  });
+}
+/*
+requestCrypto(priceURLTest, function(body) {
+  console.log(JSON.parse(body));
+});
+*/
