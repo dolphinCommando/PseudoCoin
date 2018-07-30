9fe361111d8758c367e36397f932d011e02cb080
Neeni Nagaraj <neeninagaraj@neenis-air.attlocal.net>
2018-07-30 16:02:34 -0400

    updates to NotifButton and Cards components, and to Login and Notifications pages

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -8,7 +8,7 @@ const Cards = (props) => {
             <div className="card-body">
                 <h5 className="card-title">Cryptocurrency</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/wallet" className="btn btn-primary">Go to wallet</a>
+                <a href="/wallet" className="btn btn-primary">Go to Wallet</a>
             </div>
         </div>
     )
--- client/src/pages/Notifications/Notifications.js
+++ client/src/pages/Notifications/Notifications.js
@@ -14,24 +14,43 @@ class Notifications extends React.Component {
         });
         let notificationView = 
         <ul>
-            <li className="header-notifs active">
-            <i className="material-icons">&#xe7f4;</i>
-            <ul className="dropdown-notifs"> 
-                <li className="dropdown-notifs-title"> 
-                    <button onClick={this.props.onMarkedNotificationsRead}>Mark All As Read</button>
+            {/* <div className="container">
+                <p>Feel free to close notifications once you're done reading them!</p>
+                <hr/>
+                <div className="notifications-dashboard">
+                    <div className="tile" draggable="true"/>
+                </div> */}
+            <div className="dropdown">    
+                <li className="header-notifs active">
+                {/* <i className="material-icons">&#xe7f4;</i> */}
+                <ul className="dropdown-notifs"> 
+                    <li className="dropdown-notifs-title">
+                        {notifsLi} 
+                        <button onClick={this.props.onMarkedNotificationsRead}>Mark All As Read</button>
+                    </li> 
+                    <li className="dropdown-notifs-see-all">
+                        <a href="/notifications">See All Notifications</a>
+                    </li>
+                </ul>
                 </li>
-                {notifsLi}  
-                <li className="dropdown-notifs-see-all">
-                    <a href="/notifications">See All Notifications</a>
-                </li>
-            </ul>
-            </li>
+            </div>
+            {/* <div class="dropdown show">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Notifications
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div> */}
+            {/* </div> */}
         </ul>
         return (   
         <nav className="header-nav-dropdown">
             <h3>Notifications</h3>
-            {this.props.notificationManager.isUnreadNofications() ? notificationView : <h4>List is Empty</h4>}
-                
+            {this.props.notificationManager.isUnreadNotifications() ? notificationView : <h4>List is Empty</h4>}  
         </nav>
     );
   }  
