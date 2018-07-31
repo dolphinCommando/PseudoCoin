50457308e55a5a0a2a8e01aed3968cd09a0f54cc
Neeni Nagaraj <neeninagaraj@neenis-air.attlocal.net>
2018-07-30 21:12:06 -0400

    final touches to Login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -7,37 +7,45 @@ const Login = props => {
                 <div className="content auto expand-mobile">
                     <div className="signout-block expand-mobile">
                         <div className="row header">
-                            <div className="login-title">
-                                <h1>Member Login</h1>
+                            <div className="col-12">
+                                <div className="login-title">
+                                    <h1 className="login-title">GAME OF COINS</h1>
+                                </div> 
                             </div>
+                            <br/>
+                            <div className="col-lg-8">
                             <div className="login-form">
                                 <form action="form">
                                     <div className="container">
                                         <div className="row">
-                                            <div className="one-column inner">
+                                            <div className="col-lg-8">
                                                 <label for="uname-input"><b>Username</b></label>
-                                                <input type="text" placeholder="Enter Username" name="uname-input" required />
+                                                <br/>
+                                                <input type="text" placeholder="Jon Snow" name="uname-input" required />
                                             </div>
                                         </div>
-                                        <br/>
                                         <br/>
                                         <div className="row">
-                                            <div className="one-column inner">
+                                            <div className="col-lg-8">
                                                 <label for="psw-input"><b>Password</b></label>
-                                                <input type="password" placeholder="Enter Password" name="psw-input" required />
+                                                <br/>
+                                                <input type="password" placeholder="Winter is Coming" name="psw-input" required />
                                             </div>
                                         </div>
-        
-                                        <button type="submit">Login</button>
+                                        <br/>
+                                        <button type="submit" className="w3-button w3-red w3-round-large w3-round-size">Login</button>
                                         <label for="remember-input"></label>
+                                        <br/>
                                         <input type="checkbox" checked="checked" name="remember-input"/> Remember me
                                     </div>
-
+                                    <br/>
                                     <div className="container" styles="background-color:#f1f1f1">
-                                        <button type="button" className="cancelbtn">Cancel</button>
+                                        <button type="button" className="cancelbtn w3-button w3-red w3-round-large w3-round-size">Cancel</button>
+                                        <br/>
                                         <span className="psw">Forgot <a href="#">password?</a></span>
                                     </div>
                                 </form>
+                            </div>
                             </div>
                         </div>
                     </div>
